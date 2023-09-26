899e2e03ae13b56c8792d6d816388f849ee50eda
quietgong <quietgong@naver.com>
2023-09-26 16:05:00 +0900

    chromata

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_DVvGspnBIxWizHKfODNnKUYzP1OJpbg",
+  authDomain: "chromata-5f146.firebaseapp.com",
+  projectId: "chromata-5f146",
+  storageBucket: "chromata-5f146.appspot.com",
+  messagingSenderId: "791332174912",
+  appId: "1:791332174912:web:774aeb88539d30eb6af3a5",
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
